9ce2ea9f0b2ac7eae2d94602475b035fcf836f43
dxy <150582242+yndxy@users.noreply.github.com>
2026-09-25 21:43:53 +0800

    Update diy-part1.sh

--- 24.10/diy-part1.sh
+++ 24.10/diy-part1.sh
@@ -56,7 +56,7 @@ git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/l
 # Daed
 # rm -rf package/*/daed package/*/luci-app-daed feeds/*/daed feeds/*/luci-app-daed
 # find ./ -name "Makefile" | grep -E "daed|dae|luci-app-dae|luci-app-daed" | xargs rm -f
-find ./ -name "Makefile" | grep -E "/(daed|dae|luci-app-dae|luci-app-daed)/" | xargs rm -f
+find ./ -name "Makefile" | grep -E "/(daed|dae|luci-app-dae)/" | xargs rm -f
 git clone --depth=1 https://github.com/kenzok8/openwrt-daede.git package/custom/daede
 # 添加 vmlinux-btf 模块
 # git clone https://github.com/kenzok8/vmlinux-btf.git package/custom/vmlinux-btf
