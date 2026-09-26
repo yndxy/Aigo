993cbf1979c320d972db4b2b4abeec809a2bc74a
dxy <150582242+yndxy@users.noreply.github.com>
2026-09-26 23:24:46 +0800

    Update diy-part4.sh

--- 24.10/diy-part4.sh
+++ 24.10/diy-part4.sh
@@ -14,43 +14,41 @@ mkdir -p package/custom
 
 # 科学插件
 # Passwall
-rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,daed,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,dae,daed,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 # git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git package/custom/passwall-packages
 # rm -rf feeds/luci/applications/luci-app-passwall
 # git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall.git package/custom/passwall
 # git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall2.git package/custom/passwall2
 
 # OpenClash
-rm -rf feeds/luci/applications/luci-app-openclash
-git clone --depth=1 -b dev https://github.com/vernesong/OpenClash.git package/custom/openclash
+# rm -rf feeds/luci/applications/luci-app-openclash
+# git clone --depth=1 -b dev https://github.com/vernesong/OpenClash.git package/custom/openclash
 
 # Nikki / Momo
-git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki.git package/custom/nikki
-git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-momo.git package/custom/momo
+# git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki.git package/custom/nikki
+# git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-momo.git package/custom/momo
 
 # Daed
-rm -rf feeds/luci/applications/luci-app-daed
 git clone --depth=1 https://github.com/kenzok8/openwrt-daede.git package/custom/daede
 
 # SSR+
-git clone --depth=1 https://github.com/fw876/helloworld.git package/custom/ssrp
+# git clone --depth=1 https://github.com/fw876/helloworld.git package/custom/ssrp
 
 # 功能插件
-git clone --depth=1 https://github.com/sirpdboy/luci-app-poweroffdevice.git package/custom/poweroffdevice
-git clone --depth=1 https://github.com/isalikai/luci-app-owq-wol.git package/custom/owq-wol
+# git clone --depth=1 https://github.com/sirpdboy/luci-app-poweroffdevice.git package/custom/poweroffdevice
+# git clone --depth=1 https://github.com/isalikai/luci-app-owq-wol.git package/custom/owq-wol
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/custom/lucky
-git clone --depth=1 https://github.com/sbwml/luci-app-openlist2.git package/custom/openlist2
+# git clone --depth=1 https://github.com/sbwml/luci-app-openlist2.git package/custom/openlist2
 
-git clone --depth=1 https://github.com/stackia/rtp2httpd.git package/custom/rtp2httpd
+# git clone --depth=1 https://github.com/stackia/rtp2httpd.git package/custom/rtp2httpd
 
-git clone --depth=1 https://github.com/sirpdboy/luci-app-watchdog.git package/custom/watchdog
-git clone --depth=1 https://github.com/sirpdboy/luci-app-taskplan.git package/custom/taskplan
-git clone --depth=1 https://github.com/iv7777/luci-app-authshield.git package/custom/authshield
-git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/custom/OpenAppFilter
-git clone --depth=1 https://github.com/janvanstiphout/luci-app-accesscontrol.git package/custom/accesscontrol
+# git clone --depth=1 https://github.com/sirpdboy/luci-app-watchdog.git package/custom/watchdog
+# git clone --depth=1 https://github.com/sirpdboy/luci-app-taskplan.git package/custom/taskplan
+# git clone --depth=1 https://github.com/iv7777/luci-app-authshield.git package/custom/authshield
+# git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/custom/OpenAppFilter
+# git clone --depth=1 https://github.com/janvanstiphout/luci-app-accesscontrol.git package/custom/accesscontrol
 
 # 升级替换 mosdns
-# drop mosdns and v2ray-geodata packages that come with the source
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 
@@ -61,24 +59,24 @@ rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 
 # 升级替换 smartdns
-WORKINGDIR="`pwd`/feeds/packages/net/smartdns"
-mkdir $WORKINGDIR -p
-rm $WORKINGDIR/* -fr
-wget https://github.com/pymumu/openwrt-smartdns/archive/master.zip -O $WORKINGDIR/master.zip
-unzip $WORKINGDIR/master.zip -d $WORKINGDIR
-mv $WORKINGDIR/openwrt-smartdns-master/* $WORKINGDIR/
-rmdir $WORKINGDIR/openwrt-smartdns-master
-rm $WORKINGDIR/master.zip
+# WORKINGDIR="`pwd`/feeds/packages/net/smartdns"
+# mkdir $WORKINGDIR -p
+# rm $WORKINGDIR/* -fr
+# wget https://github.com/pymumu/openwrt-smartdns/archive/master.zip -O $WORKINGDIR/master.zip
+# unzip $WORKINGDIR/master.zip -d $WORKINGDIR
+# mv $WORKINGDIR/openwrt-smartdns-master/* $WORKINGDIR/
+# rmdir $WORKINGDIR/openwrt-smartdns-master
+# rm $WORKINGDIR/master.zip
 
-LUCIBRANCH="master" #更换此变量
-WORKINGDIR="`pwd`/feeds/luci/applications/luci-app-smartdns"
-mkdir $WORKINGDIR -p
-rm $WORKINGDIR/* -fr
-wget https://github.com/pymumu/luci-app-smartdns/archive/${LUCIBRANCH}.zip -O $WORKINGDIR/${LUCIBRANCH}.zip
-unzip $WORKINGDIR/${LUCIBRANCH}.zip -d $WORKINGDIR
-mv $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}/* $WORKINGDIR/
-rmdir $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}
-rm $WORKINGDIR/${LUCIBRANCH}.zip
+# LUCIBRANCH="master" #更换此变量
+# WORKINGDIR="`pwd`/feeds/luci/applications/luci-app-smartdns"
+# mkdir $WORKINGDIR -p
+# rm $WORKINGDIR/* -fr
+# wget https://github.com/pymumu/luci-app-smartdns/archive/${LUCIBRANCH}.zip -O $WORKINGDIR/${LUCIBRANCH}.zip
+# unzip $WORKINGDIR/${LUCIBRANCH}.zip -d $WORKINGDIR
+# mv $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}/* $WORKINGDIR/
+# rmdir $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}
+# rm $WORKINGDIR/${LUCIBRANCH}.zip
 
 # VPN
 # git clone --depth=1 https://github.com/EasyTier/luci-app-easytier.git package/custom/easytier
